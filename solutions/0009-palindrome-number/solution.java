975623bef37391c21b46f8d23394097722a0cc0b
Akil26 <akil20052622@gmail.com>
2025-01-30 03:45:49 +0000

    Sync LeetCode submission Runtime - 5 ms (78.99%), Memory - 44.1 MB (99.97%)

---
+++ solutions/0009-palindrome-number/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x == 0){
+            return true;
+        }
+        if(x < 0 || x % 10 == 0){
+            return false;
+        }
+        int reverse_int = 0;
+        while(x > reverse_int){
+            int pop = x%10;
+            x/=10;
+            reverse_int = (reverse_int * 10) + pop;
+        }
+        if(x == reverse_int || x== reverse_int / 10)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
