36a45fff56b2900023640dd1a6b45383e3576cc7
Akil26 <akil20052622@gmail.com>
2025-10-31 16:47:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.9 MB (100.00%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,9 +1,8 @@
-from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dec = {}
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in dec:
-                return [dec[diff],i]
-            dec[nums[i]] = i 
+            deff = target-nums[i]
+            if deff in dec:
+                return [dec[deff],i]
+            dec[nums[i]] = i
