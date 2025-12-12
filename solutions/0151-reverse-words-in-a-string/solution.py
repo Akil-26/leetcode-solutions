4b22ce8a3252096ec0fac6d3f775713d4c335534
Akil26 <akil20052622@gmail.com>
2025-12-12 05:27:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

---
+++ solutions/0151-reverse-words-in-a-string/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s= s.strip().split()
+        res= ""
+        for i in range(len(s)-1,-1,-1):
+            res+=s[i]+" "
+        return res.strip()
