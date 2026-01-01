df96ff7a45669740f4c6ae766dfeda941945e439
Akil26 <akil20052622@gmail.com>
2026-01-01 06:23:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.3 MB (100.00%)

--- solutions/0066-plus-one/solution.py
+++ solutions/0066-plus-one/solution.py
@@ -1,5 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        res = int("".join(map(str, digits)))
-        res += 1
-        return list(map(int, str(res)))
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+        # under this code gives extra space because u will change int and string
+        """res = ''
+        for i in digits:
+            res += str(i)
+        return list(int(i) for i in str(int(res)+1))"""
