fc50cf1b78d93c1d223c725669386461049cad10
Akil26 <akil20052622@gmail.com>
2026-02-15 02:12:37 +0000

    Sync LeetCode submission Runtime - 52 ms (76.04%), Memory - 23.3 MB (58.33%)

---
+++ solutions/2871-double-a-number-represented-as-a-linked-list/solution.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head):
+            prev = None
+            curr = head
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            return prev
+        head = reverse(head)
+        curr = head
+        carry = 0
+        prev = None
+        while curr:
+            total = curr.val * 2 + carry
+            curr.val = total % 10
+            carry = total // 10
+            prev = curr
+            curr = curr.next
+        # if carry remains, add new node
+        if carry:
+            prev.next = ListNode(carry)
+        
+        return reverse(head)
+
