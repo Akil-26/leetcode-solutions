e335034997e62157bd4fd6f52e1348a28063d2ad
Akil26 <akil20052622@gmail.com>
2026-02-26 05:09:31 +0000

    Sync LeetCode submission Runtime - 1448 ms (37.93%), Memory - 0.0B (100.00%)

---
+++ solutions/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT v.customer_id, COUNT(v.customer_id) AS count_no_trans
+FROM visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
