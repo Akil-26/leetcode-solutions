79ae125ba55119e1abf7cba31e16627d9652e233
Akil26 <akil20052622@gmail.com>
2026-03-01 05:54:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (84.84%)

---
+++ solutions/4229-trim-trailing-vowels/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def trimTrailingVowels(self, s: str) -> str:
+        for i in range(len(s)-1,-1,-1):
+            if s[i] not in 'aeiou':
+                return s[:i+1]
+        return ""
