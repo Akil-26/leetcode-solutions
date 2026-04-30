f62d3db0821f31f0593058c0b8492b696eb3255f
Akil26 <akil20052622@gmail.com>
2026-04-30 17:54:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.7 MB (10.06%)

---
+++ solutions/0046-permutations/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def find(i):
+            if i == len(nums):
+                res.append(nums[:])
+            for j in range(i,len(nums)):
+                nums[i],nums[j] = nums[j],nums[i]
+                find(i+1)
+                nums[i],nums[j] = nums[j],nums[i]
+        find(0)
+        return res
