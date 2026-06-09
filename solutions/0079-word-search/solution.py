aa5d96c83f91289302600aa6401a938cce2aacf1
Akil26 <akil20052622@gmail.com>
2026-06-09 17:05:44 +0000

    Sync LeetCode submission Runtime - 3910 ms (50.43%), Memory - 19.3 MB (82.89%)

--- solutions/0079-word-search/solution.py
+++ solutions/0079-word-search/solution.py
@@ -1,27 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def DFS(i,j,ind):
-            if ind == len(word):
+        def bt(i,j,idx):
+            if idx == len(word):
                 return True
-
-            if not (0<=i<row and 0<=j<col) or board[i][j] != word[ind]:
+            if not (0 <= i < len(board) and 0 <= j < len(board[0])):
                 return False
-                
+            if board[i][j] != word[idx]:
+                return False
             temp = board[i][j]
-            board[i][j] = '#'
-            found = (
-                     DFS(i+1,j,ind+1) or 
-                     DFS(i-1,j,ind+1) or 
-                     DFS(i,j+1,ind+1) or 
-                     DFS(i,j-1,ind+1)
-                    )
+            board[i][j] = "."
+            if bt(i+1,j,idx+1):
+                return True
+            if bt(i-1,j,idx+1):
+                return True
+            if bt(i,j+1,idx+1):
+                return True
+            if bt(i,j-1,idx+1):
+                return True
             board[i][j] = temp
-            return found
-        row = len(board)
-        col = len(board[0])
-        for i in range(row):
-            for j in range(col):
-                if board[i][j] == word[0]:
-                    if DFS(i,j,0):
-                        return True
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if bt(i,j,0):
+                    return True
         return False
