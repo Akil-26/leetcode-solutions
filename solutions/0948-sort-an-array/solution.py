05bfe447d604ba00097cfc16952ac06032d234c2
Akil26 <akil20052622@gmail.com>
2026-06-11 16:51:27 +0000

    Sync LeetCode submission Runtime - 26 ms (90.95%), Memory - 26 MB (98.36%)

--- solutions/0948-sort-an-array/solution.py
+++ solutions/0948-sort-an-array/solution.py
@@ -1,29 +1,4 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        if len(nums)>1:
-            mid=len(nums)//2
-            left=nums[:mid]
-            right=nums[mid:]
-
-            self.sortArray(left)
-            self.sortArray(right)
-
-            lp=rp=fp=0
-
-            while lp<len(left) and rp<len(right):
-                if left[lp]<right[rp]:
-                    nums[fp]=left[lp]
-                    lp+=1
-                else:
-                    nums[fp]=right[rp]
-                    rp+=1
-                fp+=1
-            while lp<len(left):
-                nums[fp]=left[lp]
-                lp+=1
-                fp+=1
-            while rp<len(right):
-                nums[fp]=right[rp]
-                rp+=1
-                fp+=1
+        nums.sort()
         return nums
